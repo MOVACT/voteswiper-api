74252db15311da6d05b255aa09411f9c9a10256b
Max Mitschke <max@movact.de>
2021-05-27 11:03:24 +0200

    add prod cdn url

--- resources/js/config/app.ts
+++ resources/js/config/app.ts
@@ -2,7 +2,7 @@ const app = {
     assetsPublicUrl:
         process.env.NODE_ENV === 'development'
             ? 'https://voteswiper-uploads-dev.s3.eu-central-1.amazonaws.com/'
-            : '',
+            : 'https://cdn.voteswiper.org/',
 };
 
 export default app;
